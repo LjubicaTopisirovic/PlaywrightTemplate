2e036d929b2b2211f1529c3b48d215a88eb7d85c
Ljubica Topisirovic <ljubica.topisirovic@htecgroup.com>
2025-10-06 11:48:57 +0200

    baseURL added to the file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv'; //brisi
-import path from 'path'; //brisi
+import dotenv from 'dotenv'; 
+import path from 'path'; 
+
+dotenv.config({ 
+  path: path.resolve(__dirname, '.env') 
+}); 
 
 /**
  * Read environment variables from file.
@@ -9,11 +13,6 @@ import path from 'path'; //brisi
 // import dotenv from 'dotenv';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-// Load environment variables from .env file
-dotenv.config({ 
-  path: path.resolve(__dirname, '.env') 
-}); //brisi
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -33,7 +32,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASE_URL, //brisi
+    baseURL: process.env.BASE_URL, 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
